0713be05eb2e02269e7849cd87c27eb742471aaf
Ken Van Haren <kenvanharen@gmail.com>
2017-03-23 11:11:20 -0700

    function stubs

--- match/datatypes.py
+++ match/datatypes.py
@@ -1,15 +1,31 @@
+import inspect
+from .utils import memoize
 
 
 class DataType(object):
+    super_types = []
+
+class StringDataType(object):
     pass
 
-class PhoneNumberType(DataType):
-    pass
+class PhoneNumberType(StringDataType):
+    super_types = [StringDataType]
 
-class FullNameType(DataType):
-    pass
+class FullNameType(StringDataType):
+    super_types = [StringDataType]
 
 
 DATATYPES = {
-    'phone_number': PhoneNumberType
+    'phone_number': PhoneNumberType,
+    'full_name': FullNameType,
 }
+
+
+@memoize
+def lowest_common_type(t1, t2):
+    if t1 == t2:
+        return t1
+    for t in t1.super_types:
+        if t in t2.super_types:
+            return t
+    return None
--- match/match.py
+++ match/match.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+from .datatypes import DATATYPES, lowest_common_type
+
+
+def detect_type(s):
+    return 0, None
+
+
+def score(s1, s2):
+    s1_type = detect_type(s1)
+    s2_type = detect_type(s2)
+    dtype = lowest_common_type(s1_type, s2_type)
+    return score_for_type(s1, s2, dtype)
+
+
+def score_for_type(s1, s2, dtype):
+    return 0, dtype
