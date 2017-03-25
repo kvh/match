e18ee6601297d61ecb03f155f81f73e5342a83ba
Ken Van Haren <kenvanharen@gmail.com>
2017-03-24 21:20:06 -0700

    api refactor

--- match/match.py
+++ match/match.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from .datatypes import closest_common_type, eligible_types
+from .datatypes import (
+    closest_common_type,
+    eligible_types,
+    get_datatype)
 
 
 def score_types(s, dtypes):
@@ -18,13 +21,16 @@ def detect_type(s):
     return best
 
 
-def score(s1, s2):
+def score_similarity(s1, s2):
     score, s1_type = detect_type(s1)
     score, s2_type = detect_type(s2)
     dtype = closest_common_type(s1_type, s2_type)
-    return score_for_type(s1, s2, dtype), dtype
+    return score_similarity_as_type(s1, s2, dtype), dtype
 
 
-def score_for_type(s1, s2, dtype):
-    score = dtype().score_similarity(s1, s2)
+def score_similarity_as_type(s1, s2, dtype):
+    score = get_datatype(dtype).score_similarity(s1, s2)
     return score
+
+def parse_as_type(s, dtype, to_object=False):
+    return get_datatype(dtype).parse(s, to_object=to_object)
